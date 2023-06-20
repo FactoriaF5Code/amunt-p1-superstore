079c8ce015e54ac938c98ffc678818a020836043
Manuraptor <majejuma@hotmail.com>
2023-06-20 15:09:59 +0200

    cosa1

--- web-client-backoffice/src/lib/components/ProductCard.svelte
+++ web-client-backoffice/src/lib/components/ProductCard.svelte
@@ -1,14 +1,19 @@
 <script>
     import missingProductImage from "../../assets/missingProductImage.png"
     import {navigate} from "svelte-routing";
+    import imagen from "../../assets/imagenes/39418.jpg"
 
     export let id;
     export let name;
     export let price;
     export let imageUrl = missingProductImage;
+
+
+
     let handleClick = () => {
         navigate(`/products/${id}`)
     }
+
 </script>
 
 <article on:click={handleClick}>
@@ -16,22 +21,35 @@
     <div>
         <p>{name}</p>
         <p>{price}€</p>
+        <button><img src={imagen}></button>
     </div>
 </article>
 
 <style>
+
+
+
+    button {
+        width: 60px;
+        height: 60px;
+
+    }
+
+
+
     article {
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         background-color: white;
-        width: 250px;
+        width: 290px;
         height: 300px;
         border-radius: 5px;
         cursor: pointer;
         -webkit-box-shadow: -1px 1px 5px 0px #727171;
         -moz-box-shadow: -1px 1px 5px 0px #727171FF;
         box-shadow: -1px 1px 5px 0px #727171FF;
+        padding: 1px;
     }
 
     article:hover {
@@ -47,6 +65,9 @@
 
     div {
         padding: 15px;
+        display: flex;
+       justify-content: space-between;
+
     }
 </style>
 
